a25fa4c57c9177483e675e1b185a8deb4f3dafb4
Stimolo  Riccardo <rikisti007@gmail.com>
2026-01-16 10:59:47 +0100

    creazione pagine di accesso e registrazione

---
+++ blackJack_evoluto/blackJack_evoluto/Giocatore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackJack_evoluto
+{
+    internal class Giocatore
+    {
+        private string nomeUtente, password, email, numeroTelefono, nome, cognome;
+        private double saldo;
+        private int numeroVittorie;
+
+        public Giocatore(string nome, string cognome, string nomeUtente, string email, string numeroTelefono, string password, double saldo)
+        {
+
+        }
+    }
+}
---
+++ blackJack_evoluto/blackJack_evoluto/PaginaRegistrazione.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace blackJack_evoluto
+{
+    /// <summary>
+    /// Logica di interazione per PaginaRegistrazione.xaml
+    /// </summary>
+    public partial class PaginaRegistrazione : Window
+    {
+        Giocatore g;
+        public PaginaRegistrazione()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String riga = txtBox_nome.Text + ";" + txtBox_cognome.Text + ";" + txtBox_email.Text + ";" + txtBox_nomeUtente + ";" + txtBox_numeroTelefono;
+            String[] campi = riga.Split(';');
+        }
+    }
+}
